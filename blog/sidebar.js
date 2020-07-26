aca5fe4335b1ab9b3f65fe8f076d68227bef62ff
howard.reith@gmail.com <howard.reith@gmail.com>
2020-07-26 13:51:29 -0400

    Add git best practices video

---
+++ blog/sidebar.js
@@ -0,0 +1,100 @@
+document.write('\
+\
+    <div id="sidebar" class="sidebar nobottommargin col_last clearfix">\
+    <div class="sidebar-widgets-wrap">\
+\
+        <div class="widget clearfix">\
+\
+            <div class="tabs nobottommargin clearfix" id="sidebar-tabs">\
+\
+                <ul class="tab-nav clearfix">\
+                    <li><a href="#tabs-1">Popular</a></li>\
+                    <li><a href="#tabs-2">Recent</a></li>\
+                </ul>\
+\
+                <div class="tab-container">\
+\
+                    <div class="tab-content clearfix" id="tabs-1">\
+                        <div id="popular-post-list-sidebar">\
+\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="git-best-practices.html">\
+                                            Understanding Git and Best Practices\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="understanding-asynchrony.html">\
+                                            Understanding Asynchrony in Javascript\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="announcing-the-howie-code-blog.html">\
+                                            Announcing the Howie Reith Code Blog\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                    <div class="tab-content clearfix" id="tabs-2">\
+                        <div id="recent-post-list-sidebar">\
+                        \
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="git-best-practices.html">\
+                                            Understanding Git and Best Practices\
+                                        </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>26th July 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="understanding-asynchrony.html">\
+                                            Understanding Asynchrony in Javascript\
+                                        </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>19th July 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4>\
+                                            <a href="announcing-the-howie-code-blog.html">\
+                                                Announcing the Howie Reith Code Blog\
+                                            </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>4th July 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="widget clearfix">\
+        </div>\
+    </div>\
+</div>\
+');
