f92f88e4be558e18e370c7762d581685daa4e88b
howard.reith@gmail.com <howard.reith@gmail.com>
2020-08-09 18:46:40 -0400

    Add three blog entries and the voice section

---
+++ guyde/blog/sidebar.js
@@ -0,0 +1,97 @@
+document.write('\
+\
+    <div id="sidebar" class="sidebar nobottommargin col_last clearfix">\
+    <div class="sidebar-widgets-wrap">\
+\
+        <div class="widget clearfix">\
+\
+            <div class="tabs nobottommargin clearfix" id="sidebar-tabs">\
+\
+                <ul class="tab-nav clearfix">\
+                    <li><a href="#tabs-1">Popular</a></li>\
+                    <li><a href="#tabs-2">Recent</a></li>\
+                </ul>\
+\
+                <div class="tab-container">\
+\
+                    <div class="tab-content clearfix" id="tabs-1">\
+                        <div id="popular-post-list-sidebar">\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="charismatic-voice.html">\
+                                            How to Captivate People with a Charismatic Voice\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="voice-videos.html">\
+                                            Voice Videos are Up\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                            \
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="the-guyde-introduction.html">\
+                                            The Guyde - Introduction\
+                                        </a></h4>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                    <div class="tab-content clearfix" id="tabs-2">\
+                        <div id="recent-post-list-sidebar">\
+                          <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="voice-videos.html">\
+                                            Voice Videos are Up\
+                                        </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>August 9, 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="charismatic-voice.html">\
+                                            How to Captivate People with a Charismatic Voice\
+                                        </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>August 9, 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                            <div class="spost clearfix">\
+                                <div class="entry-c">\
+                                    <div class="entry-title">\
+                                        <h4><a href="the-guyde-introduction.html">\
+                                            The Guyde - Introduction\
+                                        </a></h4>\
+                                    </div>\
+                                    <ul class="entry-meta">\
+                                        <li>9th August 2020</li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="widget clearfix">\
+        </div>\
+    </div>\
+</div>\
+');
